c9d56b6a8815c6bf529532f4520f85b31f6ee363
MatheusBenestorff <matheusbenestorff@gmail.com>
2026-02-27 20:17:57 -0300

    Update Map constructor to use dinamic maps

--- src/World/Map.cs
+++ src/World/Map.cs
@@ -3,37 +3,66 @@ namespace PacMan
     public class Map
     {
         private const string WALL = "#";
-        private const char POINT = '.';
+        private const string POINT = ".";
         private const string EMPTY_SPACE = " ";
         public readonly int Height;
         public readonly int Width;
 
         private readonly string[,] _grid;
 
-        public Map()
+        public int PlayerSpawnX { get; private set; }
+        public int PlayerSpawnY { get; private set; }
+
+        public int EnemySpawnX { get; private set; }
+        public int EnemySpawnY { get; private set; }
+
+        public Map(string filePath)
         {
-            //Tamanho do mapa
-            this.Height = 20;
-            this.Width = 35;
+            string[] lines = File.ReadAllLines(filePath);
+
+            // Define as dimensões dinamicamente baseadas no arquivo
+            this.Height = lines.Length;
+            this.Width = lines[0].Length; 
+            
             _grid = new string[this.Height, this.Width];
 
-            //Preencher o grid
             for (int line = 0; line < this.Height; line++)
             {
                 for (int column = 0; column < this.Width; column++)
                 {
-                    if (line == 0 || line == this.Height - 1 || column == 0 || column == this.Width - 1)
+                    char fileChar = lines[line][column];
+
+                    switch (fileChar)
                     {
-                        _grid[line, column] = WALL;
-                    }
-                    else
-                    {
-                        _grid[line, column] = EMPTY_SPACE;
+                        case 'H':
+                            _grid[line, column] = WALL;
+                            break;
+                        case '.':
+                            _grid[line, column] = POINT;
+                            break;
+                        case ' ':
+                            _grid[line, column] = EMPTY_SPACE;
+                            break;
+                        
+                        case '<':
+                            this.PlayerSpawnX = column;
+                            this.PlayerSpawnY = line;
+                            _grid[line, column] = EMPTY_SPACE;
+                            break;
+                        
+                        case 'o':
+                            this.EnemySpawnX = column;
+                            this.EnemySpawnY = line;
+                            _grid[line, column] = EMPTY_SPACE;
+                            break;
+                        
+                        default:
+                            _grid[line, column] = EMPTY_SPACE; 
+                            break;
                     }
                 }
             }
         }
-
 
         //Retorna o caractere do mapa em uma coordenada específica.
         public string GetTileAt(int line, int column)
