40ae7c95ba0accb885f467f5d96cddeb14ff520d
MatheusBenestorff <matheusbenestorff@gmail.com>
2026-03-04 23:12:23 -0300

    Add Clyde move logic

--- src/Entities/Ghosts/Clyde.cs
+++ src/Entities/Ghosts/Clyde.cs
@@ -4,8 +4,29 @@ namespace PacMan
     {
         public Clyde(Map gameMap, PacMan pacman) : base(gameMap, pacman)
         {
-            this.Color = ConsoleColor.DarkYellow;
+            this.Color = ConsoleColor.DarkYellow; 
+        }
+
+        public override void Move()
+        {
+            double distanceToPacman = Math.Sqrt(
+                Math.Pow(_pacman.CurrentPositionX - this.CurrentPositionX, 2) + 
+                Math.Pow(_pacman.CurrentPositionY - this.CurrentPositionY, 2)
+            );
+
+            if (distanceToPacman > 8)
+            {
+                // It's far away: Pretend is Blinky and attack
+                MoveTowardsTarget(_pacman.CurrentPositionX, _pacman.CurrentPositionY);
+            }
+            else
+            {
+                // It's close: Run to his safe corner
+                int safeCornerX = 0;
+                int safeCornerY = _gameMap.Height - 1;
+
+                MoveTowardsTarget(safeCornerX, safeCornerY);
+            }
         }
     }
-
 }
