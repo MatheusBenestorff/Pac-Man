b81391e7c27656173b420d40c9356b9ff26ee6fe
MatheusBenestorff <matheusbenestorff@gmail.com>
2026-03-06 23:18:44 -0300

    Update menu ASCII art

--- src/UI/Menu.cs
+++ src/UI/Menu.cs
@@ -1,3 +1,5 @@
+using System;
+
 namespace PacMan
 {
     public class Menu
@@ -6,13 +8,21 @@ namespace PacMan
         private int SelectedOption = 0;
 
         private bool _saveExists;
+        private int _baseStartY;
 
         public int Show()
         {
             _saveExists = SaveSystem.SaveFileExists();
 
+            if (!_saveExists && SelectedOption == 1)
+            {
+                SelectedOption = 0;
+            }
+
             Console.Clear();
             Console.CursorVisible = false;
+
+            _baseStartY = Math.Max(2, (Console.WindowHeight - 24) / 2);
 
             DrawMenuArt();
 
@@ -22,7 +32,6 @@ namespace PacMan
 
                 ConsoleKeyInfo keyInfo = Console.ReadKey(true);
 
-                //PROCESSAR A TECLA PRESSIONADA
                 switch (keyInfo.Key)
                 {
                     case ConsoleKey.DownArrow:
@@ -52,47 +61,89 @@ namespace PacMan
 
         public void DrawMenuArt()
         {
-            Console.ForegroundColor = ConsoleColor.DarkYellow;
-            Console.WriteLine(@"    _ __   __ _  ___ _ __ ___   __ _ _ __  ");
-            Console.WriteLine(@"   | '_ \ / _` |/ __| '_ ` _ \ / _` | '_ \ ");
-            Console.WriteLine(@"   | |_) | (_| | (__| | | | | | (_| | | | |");
-            Console.WriteLine(@"   | .__/ \__,_|\___|_| |_| |_|\__,_|_| |_|");
-            Console.WriteLine(@"   |_|                                    ");
-            Console.ResetColor();
+            int currentY = _baseStartY;
 
-            Console.SetCursorPosition(5, 12);
-            Console.ForegroundColor = ConsoleColor.White;
-            // Console.Write("Use as setas (Cima/Baixo) e pressione Enter.");
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            WriteCentered(@"██████╗  █████╗  ██████╗      ███╗   ███╗ █████╗ ███╗   ██╗", currentY++);
+            WriteCentered(@"██╔══██╗██╔══██╗██╔════╝      ████╗ ████║██╔══██╗████╗  ██║", currentY++);
+            WriteCentered(@"██████╔╝███████║██║           ██╔████╔██║███████║██╔██╗ ██║", currentY++);
+            WriteCentered(@"██╔═══╝ ██╔══██║██║           ██║╚██╔╝██║██╔══██║██║╚██╗██║", currentY++);
+            WriteCentered(@"██║     ██║  ██║╚██████╗      ██║ ╚═╝ ██║██║  ██║██║ ╚████║", currentY++);
+            WriteCentered(@"╚═╝     ╚═╝  ╚═╝ ╚═════╝      ╚═╝     ╚═╝╚═╝  ╚═╝╚═╝  ╚═══╝", currentY++);
+
+            currentY += 3; 
+
+            int chaseSceneWidth = 66; 
+            int startX = Math.Max(0, (Console.WindowWidth - chaseSceneWidth) / 2);
+
+            Console.SetCursorPosition(startX, currentY);
+            Console.ForegroundColor = ConsoleColor.Yellow; Console.Write(@"  CCCC                         ");
+            Console.ForegroundColor = ConsoleColor.Red; Console.Write(@"/    \  ");
+            Console.ForegroundColor = ConsoleColor.Magenta; Console.Write(@"/    \  ");
+            Console.ForegroundColor = ConsoleColor.Cyan; Console.Write(@"/    \  ");
+            Console.ForegroundColor = ConsoleColor.DarkYellow; Console.Write(@"/    \  ");
+
+            currentY++;
+            
+            Console.SetCursorPosition(startX, currentY);
+            Console.ForegroundColor = ConsoleColor.Yellow; Console.Write(@" CCC         .    .    .       ");
+            Console.ForegroundColor = ConsoleColor.Red; Console.Write(@"|o o |  ");
+            Console.ForegroundColor = ConsoleColor.Magenta; Console.Write(@"|o o |  ");
+            Console.ForegroundColor = ConsoleColor.Cyan; Console.Write(@"|o o |  ");
+            Console.ForegroundColor = ConsoleColor.DarkYellow; Console.Write(@"|o o |  ");
+
+            currentY++;
+
+            Console.SetCursorPosition(startX, currentY);
+            Console.ForegroundColor = ConsoleColor.Yellow; Console.Write(@"  CCCC                         ");
+            Console.ForegroundColor = ConsoleColor.Red; Console.Write(@"vv  vv  ");
+            Console.ForegroundColor = ConsoleColor.Magenta; Console.Write(@"vv  vv  ");
+            Console.ForegroundColor = ConsoleColor.Cyan; Console.Write(@"vv  vv  ");
+            Console.ForegroundColor = ConsoleColor.DarkYellow; Console.Write(@"vv  vv  ");
+
         }
 
         public void DrawMenuOptions()
         {
+            int optionsY = _baseStartY + 13; 
+
             for (int i = 0; i < Options.Length; i++)
             {
-                Console.SetCursorPosition(15, 7 + i);
+                string textToPrint = $"   {Options[i]}   ";
+                if (i == SelectedOption) textToPrint = $" > {Options[i]} < ";
 
-                //Continuar
+                int currentY = optionsY + (i * 2); 
+                
+                int centerX = (Console.WindowWidth / 2) - (textToPrint.Length / 2);
+                Console.SetCursorPosition(centerX, currentY);
+
                 if (i == 1 && !_saveExists)
                 {
                     Console.ForegroundColor = ConsoleColor.DarkGray;
-                    Console.Write($"  {Options[i]}  ");
+                }
+                else if (i == SelectedOption)
+                {
+                    Console.ForegroundColor = ConsoleColor.Black;
+                    Console.BackgroundColor = ConsoleColor.Yellow;
                 }
                 else
                 {
-
-                    if (i == SelectedOption)
-                    {
-                        Console.ForegroundColor = ConsoleColor.DarkBlue;
-                        Console.Write($"> {Options[i]} <");
-                    }
-                    else
-                    {
-                        Console.ForegroundColor = ConsoleColor.Gray;
-                        Console.Write($"  {Options[i]}  ");
-                    }
-                    Console.ResetColor();
+                    Console.ForegroundColor = ConsoleColor.White;
+                    Console.BackgroundColor = ConsoleColor.Black;
                 }
+
+                Console.Write(textToPrint);
+                Console.ResetColor();
             }
+        }
+
+        private void WriteCentered(string text, int y)
+        {
+            int centerX = (Console.WindowWidth / 2) - (text.Length / 2);
+            if (centerX < 0) centerX = 0; 
+            
+            Console.SetCursorPosition(centerX, y);
+            Console.Write(text);
         }
     }
 }
